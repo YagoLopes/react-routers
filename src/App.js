3e04c6b598d051857116f4e658332366fd1581eb
Yago Lopes <yagolopeslazaro@gmail.com>
2019-08-21 21:23:43 -0300

    criando estrutura de rotas basica

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./config/ReactotronConfig";
 import { Provider } from "react-redux";
 import store from "./store";
+import Routes from "./routes";
 
 console.tron.log("testando");
 
 const App = () => (
   <Provider store={store}>
-    <h1>Hello World</h1>
+    <Routes />
   </Provider>
 );
 
---
+++ src/pages/main/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+export default function main() {
+  return (
+    <div >Main</div>
+  );
+}
